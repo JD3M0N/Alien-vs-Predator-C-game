eff60faf10d2757afc9afedfe48360385d301285
JD3M0N <josuedeki21v2@gmail.com>
2024-07-14 03:23:51 +0000

    version 0.4.0.1 realized. ArchLinux working

--- Game Code/main.c
+++ Game Code/main.c
@@ -1,39 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>  // Para rand()
-#include <unistd.h>  // Para usleep()
-#include <termios.h> // Para configuración de terminal
-#include <fcntl.h>   // Para configuración de terminal
-#include <time.h>    // Para srand()
-
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 #include "game.h"
 #include "../processes/user_input_handler/input.h"
-
-int _kbhit(void)
-{
-    struct termios oldt, newt;
-    int ch;
-    int oldf;
-
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-
-    ch = getchar();
-
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-    fcntl(STDIN_FILENO, F_SETFL, oldf);
-
-    if (ch != EOF)
-    {
-        ungetc(ch, stdin);
-        return 1;
-    }
-
-    return 0;
-}
+#include "terminal_input.h"
 
 int main()
 {
---
+++ Game Code/terminal_input.h
@@ -0,0 +1,7 @@
+#ifndef TERMINAL_INPUT_H
+#define TERMINAL_INPUT_H
+
+int _kbhit(void);
+char _getch(void);
+
+#endif // TERMINAL_INPUT_H
